3517f43ed8fe955259fbd8678b797209c75606e4
LivAndre <centeno2120959@mkt.ceu.edu.ph>
2024-08-22 15:06:36 +0800

    Fixed validation in post jobs

--- backend/api/controllers/joblisting_controller.js
+++ backend/api/controllers/joblisting_controller.js
@@ -13,24 +13,23 @@ const postJobs = async (req, res, next) => {
     let company_id = req.body.company_id
     let disability_ids = req.body.disability_ids
 
-    console.log(req.body)
 
-    if(!position_name || !description || !qualification || !positiontype_id || !company_id || !disability_ids){
+    if(!position_name || !description || !qualification || !positiontype_id || !company_id || !disability_ids || disability_ids.length === 0){
         return res.status(404).json({
             successful : false,
-            message : "One or more details are missing"
+            message : "One or more details are missing, or the disability_ids array is empty"
         })
     }
     else if(util.checkSpecialChar(position_name)){
         return res.status(400).json({
             successful : false,
-            message : "Postion name must not contain special characters"
+            message : "Position name must not contain special characters"
         })
     }
     else if(!util.checkNumbers(minimum_salary) || minimum_salary < 0){
         return res.status(400).json({
             successful : false,
-            message : "Minimum Salary must only contain numbers that are greater than or equal 0"
+            message : "Minimum Salary must only contain numbers that are greater than or equal to 0"
         })
     }
     else if(!util.checkNumbers(maximum_salary) || maximum_salary <= minimum_salary){
@@ -44,7 +43,6 @@ const postJobs = async (req, res, next) => {
             const connection = await database.pool.getConnection()
     
             try {
-
                 const selectPositionTypeId = `SELECT id FROM position_type WHERE type = ?`
                 const positionTypeRows = await connection.query(selectPositionTypeId, [positiontype_id])
 
@@ -67,45 +65,34 @@ const postJobs = async (req, res, next) => {
                     const jobListingId = result.insertId 
         
                     for (let disability_id of disability_ids) {
-            
-
-                        const selectDisabilityIdQuery = `Select id FROM disability where type = ?`
+                        const selectDisabilityIdQuery = `SELECT id FROM disability WHERE type = ?`
                         const rows = await connection.query(selectDisabilityIdQuery, [disability_id])
 
                         if(rows.length === 0){
-                            const deleteCreatedJob = `Delete from job_listing where id = ?`
+                            const deleteCreatedJob = `DELETE FROM job_listing WHERE id = ?`
                             await connection.query(deleteCreatedJob, [jobListingId])
                             return res.status(400).json({
                                 successful : false, 
-                                message : "Disability Id Does not exist"
+                                message : "Disability Id does not exist"
                             })
-                        }
-                        else {
+                        } else {
                             const insertDisabilityJobQuery = `
                             INSERT INTO disability_job_listing (disability_id, joblisting_id)
                             VALUES (?, ?)
                         `
-                        await connection.query(insertDisabilityJobQuery, [rows[0].id, jobListingId])
+                            await connection.query(insertDisabilityJobQuery, [rows[0].id, jobListingId])
                         }
                     }
-        
-            
-        
+
                     return res.status(200).json({
                         successful: true,
                         message: "Job listing posted successfully!"
                     })
-                    }
-
-    
-            } 
-
-             finally {
+                }
+            } finally {
                 connection.release()
             }
-        } 
-        
-        catch (err){
+        } catch (err) {
             return res.status(500).json({
                 successful: false,
                 message: err.message
@@ -113,6 +100,7 @@ const postJobs = async (req, res, next) => {
         }
     }
 }
+
 
 
 const viewJobListing = async (req,res,next) =>{
