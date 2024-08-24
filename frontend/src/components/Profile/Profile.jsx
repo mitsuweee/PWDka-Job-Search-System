989364afaded1639343ab13b2ffb285ff98e5bbe
xcarlosmiguel <112696167+MigL12@users.noreply.github.com>
2024-08-25 00:10:30 +0800

    Profile UI Fix

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import UserProf from './UserPContent';
 import CompanyProf from './CompanyPContent';
+import Footer from '../Footer';
+import Navbar from '../Navbar';
 
 const Profile = () => {
-  // Assume the user type is stored in localStorage
+  // Assume the user type is stored in sessionStorage
   const userType = sessionStorage.getItem('Role'); // Could be 'user' or 'company'
 
-  // Render the appropriate profile based on the user type
-  if (userType === 'company') {
-    return <CompanyProf />;
-  } else if (userType === 'user') {
-    return <UserProf />;
-  } else {
-    return <div>Error: Unknown user type</div>;
-  }
+  return (
+    <div className="bg-[#E3EDF7] min-h-screen flex flex-col">
+      <header>
+        <Navbar />
+      </header>
+
+      {/* Render the appropriate profile based on the user type */}
+      <main className="flex-grow">
+        {userType === 'company' ? (
+          <CompanyProf />
+        ) : userType === 'user' ? (
+          <UserProf />
+        ) : (
+          <div>Error: Unknown user type</div>
+        )}
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Profile;
