1a7e0f8b52311cedf485aab04bf9a6e978c812e1
mitsuweee <142657685+mitsuweee@users.noreply.github.com>
2024-10-19 01:55:14 +0800

    Update connection_db.js

--- backend/api/models/connection_db.js
+++ backend/api/models/connection_db.js
@@ -4,6 +4,7 @@ const knex = require("knex")({
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
   },
   pool: { min: 0, max: 7 },
