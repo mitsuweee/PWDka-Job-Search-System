61d5146f128eb8e3993c687cd3f275929ee8410d
LivAndre <centeno2120959@mkt.ceu.edu.ph>
2024-11-12 21:33:49 +0800

    Socket URL FIXES

--- frontend/src/components/Admin/AdminViewAdmin.jsx
+++ frontend/src/components/Admin/AdminViewAdmin.jsx
@@ -19,7 +19,7 @@ const AdminViewAdmin = () => {
 
   // Initialize socket connection and listen for real-time events
   useEffect(() => {
-    const socket = io("https://pwdka.com.ph/admin/dashboard/viewadmin");
+    const socket = io("https://pwdka.com.ph:8080");
 
     socket.on("connect", () => {
       console.log("Connected to socket server with ID:", socket.id);
