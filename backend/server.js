3ac901d320a67c4061b59553c757798efe138c1c
LivAndre <centeno2120959@mkt.ceu.edu.ph>
2024-11-12 23:00:33 +0800

    Added transports in adminviewadmin

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const io = new Server(server, {
   cors: {
     origin: ["*"], // Your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   },
 });
 
